b4d4e16956df2142f14c1b3f20cc464c4b88f9b4
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 06:23:51 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,6 +10,6 @@ with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page(user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
         stealth_sync(page)
-        page.goto('https://www.manta.com/')
+        page.goto('https://www.manta.com/mb_33_C4_000/restaurants_and_bars')
         st.text(page.title())
         browser.close()
