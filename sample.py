eca596f212770a09240bbd98a0505b12d97c162e
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 04:19:27 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 import streamlit as st
 
+st.title('Starting')
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
