b9c1b1d7d8e050ee2a16b2c5eb8c8a9c1720e4a2
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 09:37:48 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,13 @@
 import os
-os.system("playwright install chromium")
+os.system("playwright install")
 
 from playwright.sync_api import sync_playwright
 import streamlit as st
 from playwright_stealth import stealth_sync
 st.title('Starting')
 with sync_playwright() as p:
-        browser = p.chromium.launch()
+        # Can be "msedge", "chrome-beta", "msedge-beta", "msedge-dev", etc.
+        browser = p.firefox.launch()
         page = browser.new_page(user_agent = "Mozilla/5.0 (Linux; Android 10; SM-G980F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.96 Mobile Safari/537.36")
         stealth_sync(page)
         page.goto('https://www.whitepages.com/business/CA/Manhattan-Beach/Restaurants')
