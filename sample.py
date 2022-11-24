7a7d39564d7a2efae7f43296afb28bca29990329
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 08:44:54 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,11 +4,10 @@ os.system("playwright install")
 from playwright.sync_api import sync_playwright
 import streamlit as st
 from playwright_stealth import stealth_sync
-ua = ["Mozilla/5.0 (Linux; Android 12; Pixel 6 Build/SD1A.210817.023; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.71 Mobile Safari/537.36", "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 6P Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36"]
 st.title('Starting')
 with sync_playwright() as p:
         browser = p.chromium.launch()
-        page = browser.new_page(ua[0])
+        page = browser.new_page("Mozilla/5.0 (Linux; Android 6.0; HTC One X10 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36")
         stealth_sync(page)
         page.goto('https://www.manta.com/c/mms0zjs/lambertville-trading-company')
         st.text(page.title())
