77c9a2699e231978a411436d48ab997fa80cf4ae
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 04:34:44 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 import os
 os.system("playwright install")
 
+from cf_clearance import sync_cf_retry, sync_stealth
 from playwright.sync_api import sync_playwright
 import streamlit as st
 
@@ -8,6 +9,17 @@ st.title('Starting')
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    page.goto("https://www.manta.com/mb_34_C432C_000/restaurants")
-    st.text(page.title())
+    sync_stealth(page, pure=True)
+    page.goto('https://nowsecure.nl')
+    res = sync_cf_retry(page)
+    if res:
+        cookies = page.context.cookies()
+        for cookie in cookies:
+            if cookie.get('name') == 'cf_clearance':
+                cf_clearance_value = cookie.get('value')
+                st.text(cf_clearance_value)
+        ua = page.evaluate('() => {return navigator.userAgent}')
+        st.text(ua)
+    else:
+        st.text("cf challenge fail")
     browser.close()
