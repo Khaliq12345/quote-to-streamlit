e6f124dc6c937396aef27e0c4b78e9207262fdbf
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 09:28:19 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -9,6 +9,6 @@ with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page(user_agent = "Mozilla/5.0 (Linux; Android 10; SM-G980F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.96 Mobile Safari/537.36")
         # stealth_sync(page)
-        page.goto('https://www.manta.com/')
+        page.goto('https://www.whitepages.com/business/CA/Manhattan-Beach/Restaurants')
         st.text(page.title())
         browser.close()
