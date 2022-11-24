8d87323dc1b7ddd37e4714fefabde4805d339928
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 04:12:56 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
+from playwright.sync_api import sync_playwright
+import streamlit as st
 
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("http://playwright.dev")
+    st.text(page.title())
+    browser.close()
