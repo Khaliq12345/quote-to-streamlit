781158a7648dfd069d977b59fcebf35a9d31385d
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 04:47:24 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,8 +7,7 @@ from playwright_stealth import stealth_sync
 
 st.title('Starting')
 with sync_playwright() as p:
-    for browser_type in [p.chromium, p.firefox, p.webkit]:
-        browser = browser_type.launch()
+        browser = p.chromium.launch()
         page = browser.new_page()
         stealth_sync(page)
         page.goto('http://whatsmyuseragent.org/')
