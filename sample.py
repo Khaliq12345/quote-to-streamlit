a5de83352b5988a6263f1ca4300090d44ed82cbe
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 08:55:38 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ from playwright_stealth import stealth_sync
 st.title('Starting')
 with sync_playwright() as p:
         browser = p.chromium.launch()
-        page = browser.new_page("Mozilla/5.0 (Linux; Android 6.0; HTC One X10 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36")
+        page = browser.new_page(user_agent = "Mozilla/5.0 (Linux; Android 6.0; HTC One X10 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36")
         stealth_sync(page)
         page.goto('https://www.manta.com/c/mms0zjs/lambertville-trading-company')
         st.text(page.title())
