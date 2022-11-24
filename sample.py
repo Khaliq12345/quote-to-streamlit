741e8cf2b95beb184b6d64fecc69b66f2207a0bd
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-24 19:04:32 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ from playwright_stealth import stealth_sync
 st.title('Starting')
 with sync_playwright() as p:
         # Can be "msedge", "chrome-beta", "msedge-beta", "msedge-dev", etc.
-        browser = p.chromium.launch()
+        browser = p.firefox.launch()
         page = browser.new_page(user_agent = "Mozilla/5.0 (Linux; Android 12; SM-S906N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile Safari/537.36")
         stealth_sync(page)
         page.goto('https://www.whitepages.com/business/CA/Manhattan-Beach/Restaurants')
