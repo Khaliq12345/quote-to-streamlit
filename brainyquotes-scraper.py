45512606fa25877061352dfdf31dcd4084ebff78
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-11-25 08:41:32 +0100

    Update brainyquotes-scraper.py

--- brainyquotes-scraper.py
+++ brainyquotes-scraper.py
@@ -1,6 +1,5 @@
 import os
 os.system("playwright install")
-from time import sleep
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -39,7 +38,6 @@ def scrape(keyword, pages):
                 'Quote': quote
                 }
                 quote_list.append(quotes)
-                sleep(2)
 
         df = pd.DataFrame(quote_list)
         return st.dataframe(df)
